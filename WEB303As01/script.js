21e224c50d11a78d96cf3e24d44c63525bf8e6db
mdarora <mdarora0001@gmail.com>
2022-09-17 15:36:46 -0400

    Calculated tech spending amount

--- WEB303As01/script.js
+++ WEB303As01/script.js
@@ -3,3 +3,16 @@
 	{Mohit Mohit}
 */
 
+$("document").ready(() => {
+
+	const annualSalary = $("#yearly-salary");
+	const techPercentage = $("#percent");
+	const techAmount = $("#amount");
+
+	function calculateTechExpense(){
+		let amount = annualSalary.val() * techPercentage.val() / 100;
+		techAmount.text(`$${amount.toFixed(2)}`);
+	}
+	annualSalary.change(calculateTechExpense);
+	techPercentage.change(calculateTechExpense);
+});
