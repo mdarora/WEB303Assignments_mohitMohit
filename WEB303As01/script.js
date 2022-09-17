b4622581f5b3d1aea4f7563c26517e3e28a93ecb
mdarora <mdarora0001@gmail.com>
2022-09-17 15:38:39 -0400

    converted change event to keyup event

--- WEB303As01/script.js
+++ WEB303As01/script.js
@@ -13,6 +13,6 @@ $("document").ready(() => {
 		let amount = annualSalary.val() * techPercentage.val() / 100;
 		techAmount.text(`$${amount.toFixed(2)}`);
 	}
-	annualSalary.change(calculateTechExpense);
-	techPercentage.change(calculateTechExpense);
+	annualSalary.keyup(calculateTechExpense);
+	techPercentage.keyup(calculateTechExpense);
 });
