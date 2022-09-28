0951a1b13fefadd5dcee6267cfa96b55940f556d
mdarora <mdarora0001@gmail.com>
2022-09-28 14:59:31 -0400

    fetching conntect from other html files using AJAX

--- WEB303As02/js/main.js
+++ WEB303As02/js/main.js
@@ -1,3 +1,31 @@
 // WEB303 Assignment 2
+// Mohit (0795486)
 
+$(function(){
+    const prospect = $('#prospect');
+    const convert = $('#convert');
+    const retain = $('#retain');
 
+    const content = $('#content');
+
+    function getContent(e) {
+        content.slideUp(500, ()=>{
+
+            const fileName = `${e.target.id}.html`;
+    
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', fileName, true);
+    
+            xhr.onload = function(){
+                content.html(this.responseText);
+                content.slideDown();
+            }
+    
+            xhr.send();
+        });
+
+    }
+    prospect.click(getContent);
+    convert.click(getContent);
+    retain.click(getContent);
+});
