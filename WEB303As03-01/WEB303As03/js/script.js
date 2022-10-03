a8e1c3f281506aff01e4c5f5e4a8e34667603269
mdarora <mdarora0001@gmail.com>
2022-10-03 01:59:22 -0400

    removed team variable because of scoping problem

--- WEB303As03-01/WEB303As03/js/script.js
+++ WEB303As03-01/WEB303As03/js/script.js
@@ -2,12 +2,11 @@ $(function(){
     getJsonData();
 });
 
-const team = $("div#team");
-
 function getJsonData(){
     $.getJSON("team.json", (result)=>{
         $.each(result, (index, data)=>{
-            team.append(`<h2>${data.name}</h2><h5>${data.position}</h5><p>${data.bio}</p>`);
+            $("div#team").append(`<h2>${data.name}</h2><h5>${data.position}</h5><p>${data.bio}</p>`);
         });
     });
 };
+
