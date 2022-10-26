91184fa76efd46bf8032b31e0bc6746e8d7ca836
mdarora <mdarora0001@gmail.com>
2022-10-26 15:36:39 -0400

    created class and constructor

--- WEB303As05/script.js
+++ WEB303As05/script.js
@@ -1,9 +1,17 @@
 /*
     Assignment 05
+    Mohit (0795486)
 */
 
 $(document).ready(function () {
-    // your code here
+    class ContentItem{
+        constructor(id, name, description, category){
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.category = category;
+        }
+    }
 
 });
 
