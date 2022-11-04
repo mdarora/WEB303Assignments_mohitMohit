1be2929bae5cec8d08a49c95451f4b2b1955a317
mdarora <mdarora0001@gmail.com>
2022-11-04 16:03:13 -0400

    Added accordian of movies of ninja

--- WEB303As06/js/script.js
+++ WEB303As06/js/script.js
@@ -1,3 +1,7 @@
 $(function(){
-   
+    $('.accordion').on('click', '.accordion-control', function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $(this).next('.accordion-panel').not(':animated').slideToggle();
+    });
 });
